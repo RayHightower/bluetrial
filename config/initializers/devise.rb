e3061e29e42e8866e6ea23a3dea218075b7d92d8
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-19 15:02:57 -0500

    add app_id and app_secret to devise.rg

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,6 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, 'f19f54036d58b5a807d3', '14ab98dca3449c5cbb72cf26bf6f33737e88adef', scope: 'user,public_repo'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
