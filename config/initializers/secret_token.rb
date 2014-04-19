2d03b59033014be2bda60b3fe452b9c8795e4c51
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-19 14:51:33 -0500

    before devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bluetrial::Application.config.secret_key_base = 'ab68b50cd2a04fb232cc9ceefa02b0b176e7060c62b51fc00d100745f64a594b23599db38ddc07698a2662d574cf8af5febc94a3c9a6bb798ca5176871210b98'
