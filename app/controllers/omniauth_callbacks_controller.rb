fc3740ed89feeaf2b4d6185fde8291c73e91d39f
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-19 15:46:13 -0500

    do more useful work within the github method

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,16 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def github
-    raise request.env['omniauth.auth'].to_yaml
+    auth = request.env['omniauth.auth'].to_yaml
+
+    user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
+
+    if user.persisted?
+      flash[:success] = "successfully logged in"
+      sign_in_and_redirect user, notice: "signed in"
+    else
+      redirect_to :root, notice: "failed to save user"
+    end
   end
 
 end
