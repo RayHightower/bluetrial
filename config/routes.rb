dadb18d4360f539774391c23daeb629abab2fc4b
Raymond T. Hightower <rth@wisdomgroup.com>
2014-04-19 15:36:50 -0500

    correct pluralization of controllers in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Bluetrial::Application.routes.draw do
 
   resources :products
 
-  devise_for :users, controller: { omniauth_callbacks: "omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
